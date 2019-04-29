a5a192ec906796370ba0d064f677410e7dd92f63
AkashBathala <akash.bathla2013@gmail.com>
2019-04-30 01:30:20 +0530

    $$$$$$$$$$

--- js/main.js
+++ js/main.js
@@ -2,45 +2,63 @@
 
 let array = [];
 var DATA = (function () {
-    topStories();
-    // bestStories();
-    // newStories();
-    function topStories() {
+
+    function topStories(load) {
+
+        let loader = `<div><h2>Please Wait while Data is Loading</h2></div>`;
+        document.getElementById('wrap').innerHTML = loader;
         fetch(`https://hacker-news.firebaseio.com/v0/topstories.json`)
             .then(function (response) {
                 return response.json();
             })
             .then(function (myJson) {
+
                 array = myJson;
-                for (let i = 0; i <= 5; i++) {
+
+                for (let i = 0; i <= load; i++) {
                     _getData(array[i]);
+                   
                 }
+                let dloader = ``;
+                document.getElementById('wrap').innerHTML = dloader;
             });
     }
-    
-    function bestStories(){
-        let beststories =[]
+
+    function bestStories() {
+        let loader = `<div><h2>Please Wait while Data is Loading</h2></div>`;
+        document.getElementById('wrap').innerHTML = loader;
+        let load = 5;
+        let bestStories = []
         fetch(`https://hacker-news.firebaseio.com/v0/beststories.json`)
             .then(function (response) {
                 return response.json();
             })
             .then(function (myJson) {
                 bestStories = myJson;
-                for (let i = 0; i <= 5; i++) {
+                for (let i = 0; i <= load; i++) {
                     _getData(bestStories[i]);
+                   
                 }
-                
+                let dloader = ``;
+                document.getElementById('wrap').innerHTML = dloader;
+
             });
     }
-    function newStories(){
+    function newStories() {
+        let loader = `<div><h2>Please Wait while Data is Loading</h2></div>`;
+        document.getElementById('wrap').innerHTML = loader;
+        let load = 5;
         let newStories
-        fetch(`https://hacker-news.firebaseio.com/v0/beststories.json`).then(function(response){
+        fetch(`https://hacker-news.firebaseio.com/v0/beststories.json`).then(function (response) {
             return response.json();
-        }).then(function(myJson){
+        }).then(function (myJson) {
             newStories = myJson;
-                for (let i = 0; i <= 5; i++) {
-                    _getData(newStories[i]);
-                }
+            for (let i = 0; i <= load; i++) {
+                _getData(newStories[i]);
+                
+            }
+            let dloader = ``;
+                document.getElementById('wrap').innerHTML = dloader;
         });
     }
     function _getData(id) {
@@ -52,78 +70,59 @@ var DATA = (function () {
             .then(function (myJson) {
                 console.log(myJson);
                 printdata(myJson)
+
+
             });
     }
-    function getarray() {
-        return array;
-
-    }
     function printdata(myJson) {
+
+
+
+        var table = document.createElement('table');
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
         var t = document.createElement('ol');
         var by = document.createElement('li');
         var descendants = document.createElement('li');
         var id = document.createElement('li');
-        var title=document.createElement('li');
+        var title = document.createElement('li');
         var kids = document.createElement('li');
         var score = document.createElement('li');
         var time = document.createElement('li');
         var type = document.createElement('li');
         var url = document.createElement('li');
 
-        title.innerHTML=`title:${myJson.title}`
+
+
+        table.id = "table";
+        title.innerHTML = `title:${myJson.title}`
         by.innerHTML = ` by: ${myJson.by}`;
         descendants.innerHTML = ` descendants: ${myJson.by}`;
         id.innerHTML = ` id: ${myJson.id}`;
         kids.innerHTML = ` kids: ${myJson.kids}`;
+        kids.style.overflow = 'hidden';
         score.innerHTML = ` score: ${myJson.score}`;
         time.innerHTML = ` time: ${myJson.time}`;
         type.innerHTML = ` type: ${myJson.type}`;
         url.innerHTML = ` url: ${myJson.url}`;
-        
-        t.appendChild(by);
-        t.appendChild(title)
-        t.appendChild(descendants);
-        t.appendChild(id);
-        t.appendChild(kids);
-        t.appendChild(score);
-        t.appendChild(time);
-        t.appendChild(type);
-        t.appendChild(url);
-        document.body.appendChild(t)
+
+
+        tr.appendChild(td);
+        td.appendChild(id);
+        td.appendChild(title);
+        td.appendChild(descendants);
+        td.appendChild(by);
+        td.appendChild(kids);
+        td.appendChild(score);
+        td.appendChild(time);
+        td.appendChild(type);
+        td.appendChild(url);
+        table.appendChild(tr);
+        document.getElementById('wrap').appendChild(table)
     }
     return {
-        // data: data,
-        getData: _getData,
-        // bestStories:bestStories
+        newStories: newStories,
+        bestStories: bestStories,
+        topStories: topStories
     }
 })()
-
-
-
-// DATA.data()
-
-
-
-
-// function getdata(id) {
-
-
-
-
-
-
-    // for (let i = 0; i < 5; i++) {
-    //     return () => {
-
-    //         fetch(`https://hacker-news.firebaseio.com/v0/item/${array[i]}.json`)
-    //             .then(function (response) {
-
-    //                 return response.json();
-    //             })
-    //             .then(function (myJson) {
-    //                 console.log(myJson);
-    //             });
-    //     }
-    // }
-// }
-
