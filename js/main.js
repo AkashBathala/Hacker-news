01a611bd3176588744dba8b9e2de140b269efab7
AkashBathala <akash.bathla2013@gmail.com>
2019-04-29 17:49:59 +0530

    $$$$

--- js/main.js
+++ js/main.js
@@ -1,33 +1,58 @@
 "use strict";
-let array = [];
 
-fetch(`https://hacker-news.firebaseio.com/v0/topstories.json`)
-    .then(function (response) {
 
-        return response.json();
-    })
-    .then(function (myJson) {
-        array = myJson;
+var DATA = (function() {    
+    let array = [];
 
-        for (let i = 0; i < 5; i++) {
-            getdata(array[i]);
-        }
-    });
-
-function getdata(id) {
-    
-    
-    // return()=>{
-    fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+    function data() {
+    fetch(`https://hacker-news.firebaseio.com/v0/topstories.json`)
         .then(function (response) {
-
             return response.json();
         })
         .then(function (myJson) {
-            console.log(myJson);
+            array = myJson;
         });
+    }
+    function _getData(id) {
+        fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                console.log(myJson);
+            });
+    }
+    return {
+        // data: data,
+        getData:_getData
+
+    }
+})()
+
+DATA.data()
+DATA.getData();
+
+// function getdata(id) {
+
+
+
+
+
+
+    // for (let i = 0; i < 5; i++) {
+    //     return () => {
+
+    //         fetch(`https://hacker-news.firebaseio.com/v0/item/${array[i]}.json`)
+    //             .then(function (response) {
+
+    //                 return response.json();
+    //             })
+    //             .then(function (myJson) {
+    //                 console.log(myJson);
+    //             });
+    //     }
     // }
-}
+// }
 
 // var t= document.createElement('ol');
 // var li=document.createElement('li');
