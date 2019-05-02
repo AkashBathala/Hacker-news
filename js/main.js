6daaf9630c51d12cda4d422579fd5e97808adce0
AkashBathala <akash.bathla2013@gmail.com>
2019-05-02 16:21:42 +0530

    $$$$$$$$$$

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 "use strict";
-
-let topStories = [];
+let words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+let topStoriesarray = [];
 let bestStories = [];
 let newStories = [];
 let arrayStoreData = [];
+
+
 let DATA = (function () {
 
-    function topStories(load) {
+    function topStories() {
 
         let loader = `<div><h2>Please Wait while Data is Loading</h2></div>`;
         document.getElementById('wrap').innerHTML = loader;
@@ -17,12 +19,20 @@ let DATA = (function () {
             })
             .then(function (myJson) {
 
-                topStories = myJson;
+                myJson.forEach(d => topStoriesarray.push(d));
+                console.log(topStoriesarray);
 
-                for (let i = 0; i <= load; i++) {
-                    _getData(topStories[i]);
+                for (let i = 0; i < 10; i++) {
+
+                    _getData(topStoriesarray[i]);
+                    console.log("in top stories", topStoriesarray[i]);
+
                 }
                 let dloader = ``;
+                let button = document.createElement('button');
+                button.innerHTML = "Load More";
+                document.getElementById('button').appendChild(button);
+                button.onclick = yhendler;
                 document.getElementById('wrap').innerHTML = dloader;
             });
     }
@@ -38,6 +48,7 @@ let DATA = (function () {
             })
             .then(function (myJson) {
                 bestStories = myJson;
+                arrayStoreData = bestStories;
                 for (let i = 0; i <= load; i++) {
                     _getData(bestStories[i]);
                 }
@@ -63,23 +74,31 @@ let DATA = (function () {
             document.getElementById('wrap').innerHTML = dloader;
         });
     }
-    function _getData(id) {
+    async function _getData(id) {
+
+        console.log("in getdata", id);
 
 
-        arrayStoreData.push(id);
         fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
             .then(function (response) {
                 return response.json();
             })
-            .then(function (myJson) {
-                console.log(myJson);
-                printdata(myJson)
-
+            .then(function (singaldata) {
+                printdata(singaldata);
+                console.log("avfter fetch in get data", id);
+                // console.log("in getdata",singaldata);
 
             });
     }
-    function printdata(myJson) {
-        /**<table>
+    function dataPrintFormat(x, y) {
+        for (let i = x; i < y; i++) {
+            // console.log(topStoriesarray[i] ,"i is",i,y);
+            _getData(topStoriesarray[i]);
+        }
+
+    }
+    async function printdata(myJson) {
+        /** <table>
      * <tr>
         <td>
             <ol>
@@ -94,7 +113,7 @@ let DATA = (function () {
         let table = document.createElement('table');
         let tr = document.createElement('tr');
         let td = document.createElement('td');
-        let ol=document.createElement('ol');
+        let ol = document.createElement('ol');
         let by = document.createElement('li');
         let descendants = document.createElement('li');
         let id = document.createElement('li');
@@ -104,9 +123,7 @@ let DATA = (function () {
         let time = document.createElement('li');
         let type = document.createElement('li');
         let url = document.createElement('li');
-    
 
-        
         table.id = "table";
         title.innerHTML = `title:${myJson.title}`
         by.innerHTML = ` by: ${myJson.by}`;
@@ -117,9 +134,9 @@ let DATA = (function () {
         time.innerHTML = ` time: ${myJson.time}`;
         type.innerHTML = ` type: ${myJson.type}`;
         url.innerHTML = ` url: ${myJson.url}`;
-        kids.id="scrollable";
-        url.id="scrollable";
 
+        kids.id = "scrollable";
+        url.id = "scrollable";
 
         tr.appendChild(td);
         td.appendChild(id);
@@ -137,6 +154,25 @@ let DATA = (function () {
     return {
         newStories: newStories,
         bestStories: bestStories,
-        topStories: topStories
+        topStories: topStories,
+        dataPrintFormat: dataPrintFormat
     }
-})()
+})();
+let starter = 10, round = 20;
+function yhendler() {
+
+    let wrap = document.getElementById('bodyid');
+    let contentheight = wrap.offsetHeight;  //gets page content height
+    let yoffset = window.scrollY;  //gets the vertical scroll positio
+    let y = yoffset + window.innerHeight;
+
+    if (y >= contentheight) {
+        // document.getElementById('wrap').innerHTML += "<div>asdasdsd</div>";
+        DATA.dataPrintFormat(starter, round);
+        starter += 10;
+        round += 10;
+    }
+    var status = document.getElementById('status');
+    status.innerHTML = contentheight + "|" + y + "|" + window.pageYOffset + "|" + window.scrollY;
+}
+
